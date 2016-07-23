4dbdddac2516446fbde8a325423c02c9c992cd33
Raul Butuc <raulbutuc@gmail.com>
2016-07-23 22:40:44 +0100

    Created project and made window untraceable

---
+++ KeyLoggerPro/KeyLoggerPro.cpp
@@ -0,0 +1,31 @@
+/**
+ * KeyLoggerPro.cpp
+ *
+ * @author Raul Butuc
+ * @version 1.0.0 - 23/07/2016
+ */
+
+#include "stdafx.h"
+#include "KeyLoggerPro.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance, 
+                      _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow) {
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
+
+  HACCEL hAccelTable =
+    LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_KEYLOGGERPRO));
+
+  MSG msg;
+
+  while (GetMessage(&msg, nullptr, 0, 0)) {
+    if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+  }
+
+  return (int)msg.wParam;
+}
---
+++ KeyLoggerPro/KeyLoggerPro.h
@@ -0,0 +1,10 @@
+/**
+ * KeyLoggerPro.h
+ *
+ * @author Raul Butuc
+ * @version 1.0.0 - 23/07/2016
+ */
+
+#pragma once
+
+#include "resource.h"
